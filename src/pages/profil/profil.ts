715f00ed860f26c280a0bc97e6b821c45732982f
Jeremy Noh <jeremynoh@MacBook-Pro-de-Wilson.local>
2018-02-02 22:12:30 +0100

    fin savePresence()

--- src/pages/profil/profil.ts
+++ src/pages/profil/profil.ts
@@ -17,7 +17,9 @@ export class ProfilPage {
   fields = this.navParams.get('fields');
   id = this.navParams.get('id');
   isValid = this.fields.Valide;
-  Vestiaire = undefined ;
+  // Vestiaire = undefined ;
+  Vestiaire = this.fields.Vestiaire;
+
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams,private http: HttpClient,private alertCtrl: AlertController,public loadingCtrl: LoadingController) {
@@ -25,131 +27,155 @@ export class ProfilPage {
 
   ionViewDidLoad() {
     console.log(this.fields);
-
+    console.log(this.fields.Vestiaire);
 
   }
 
-    funLoading (){
-      let loading = this.loadingCtrl.create({
-        content: 'Please wait...'
-      });
+  funLoading (){
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
 
-      loading.present();
+    loading.present();
 
-      setTimeout(() => {
-        loading.dismiss();
-      }, 1000);
-    }
+    setTimeout(() => {
+      loading.dismiss();
+    }, 1000);
+  }
 
 
-    DownDrink() {
+  DownDrink() {
     // console.log(this.fields);
     // console.log("apres le test");
     //   // https://api.airtable.com/v0/appRzgYd2sozz8l2P/personne?api_key=keyAER9NsfEje3klJ
     let url = `https://api.airtable.com/v0/appRzgYd2sozz8l2P/personne/${this.id}?api_key=keyAER9NsfEje3klJ`;
     this.funLoading();
     this.http.put(url,
-    {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":this.fields.Valide,"Boisson":this.fields.Boisson-1,"Nom":this.fields.Nom}})
-        .subscribe(
-            val => {
-                console.log("PUT call successful value returned in body",
-                            val);
-            },
-            response => {
-                console.log("PUT call in error", response);
-            },
-            () => {
-                this.fields.Boisson = this.fields.Boisson-1;
-                let alert = this.alertCtrl.create({
-                  title: 'Modification des Boissons',
-                  subTitle: 'Une Boison a bien été retiré!!',
-                  buttons: ['Fermer']
-                });
-                alert.present();
-            }
-        );
+      {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":this.fields.Valide,"Boisson":this.fields.Boisson-1,"Nom":this.fields.Nom}})
+      .subscribe(
+        val => {
+          console.log("PUT call successful value returned in body",
+          val);
+        },
+        response => {
+          console.log("PUT call in error", response);
+        },
+        () => {
+          this.fields.Boisson = this.fields.Boisson-1;
+          let alert = this.alertCtrl.create({
+            title: 'Modification des Boissons',
+            subTitle: 'Une Boison a bien été retiré!!',
+            buttons: ['Fermer']
+          });
+          alert.present();
+        }
+      );
 
-  }
+    }
 
-  UpDrink() {
-    let url = `https://api.airtable.com/v0/appRzgYd2sozz8l2P/personne/${this.id}?api_key=keyAER9NsfEje3klJ`;
-    // test
-    this.funLoading();
-    // fin test
-
-
-    this.http.put(url,
-    {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":this.fields.Valide,"Boisson":this.fields.Boisson+1,"Nom":this.fields.Nom}})
-        .subscribe(
-            val => {
-              // test
-              // let loading = this.loadingCtrl.create({
-              //   content: 'Please wait...'
-              // });
-              //
-              // loading.present();
-              //
-              // setTimeout(() => {
-              //   loading.dismiss();
-              // }, 3000);
-              // fin test
-                console.log("PUT call successful value returned in body",
-                            val);
-            },
-            response => {
-                console.log("PUT call in error", response);
-            },
-            () => {
-
-                console.log("Boisson augmenté");
-                this.fields.Boisson = this.fields.Boisson+1;
-                let alert = this.alertCtrl.create({
-                  title: 'Modification des Boissons',
-                  subTitle: 'Une Boison a bien été retiré!!',
-                  buttons: ['Fermer']
-                });
-                alert.present();
-                // fin
-            }
-        );
-      // fin test
-  }
-
-  SavePrence(){
+    UpDrink() {
       let url = `https://api.airtable.com/v0/appRzgYd2sozz8l2P/personne/${this.id}?api_key=keyAER9NsfEje3klJ`;
+      // test
       this.funLoading();
+      // fin test
+
+
       this.http.put(url,
-      {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":"True","Boisson":this.fields.Boisson,"Nom":this.fields.Nom}})
+        {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":this.fields.Valide,"Boisson":this.fields.Boisson+1,"Nom":this.fields.Nom}})
+        .subscribe(
+          val => {
+            // test
+            // let loading = this.loadingCtrl.create({
+            //   content: 'Please wait...'
+            // });
+            //
+            // loading.present();
+            //
+            // setTimeout(() => {
+            //   loading.dismiss();
+            // }, 3000);
+            // fin test
+            console.log("PUT call successful value returned in body",
+            val);
+          },
+          response => {
+            console.log("PUT call in error", response);
+          },
+          () => {
+
+            console.log("Boisson augmenté");
+            this.fields.Boisson = this.fields.Boisson+1;
+            let alert = this.alertCtrl.create({
+              title: 'Modification des Boissons',
+              subTitle: 'Une Boison a bien été retiré!!',
+              buttons: ['Fermer']
+            });
+            alert.present();
+            // fin
+          }
+        );
+        // fin test
+      }
+
+      SavePrence(){
+        let url = `https://api.airtable.com/v0/appRzgYd2sozz8l2P/personne/${this.id}?api_key=keyAER9NsfEje3klJ`;
+        this.funLoading();
+        this.http.put(url,
+          {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":"True","Boisson":this.fields.Boisson,"Nom":this.fields.Nom}})
           .subscribe(
+            val => {
+              console.log("PUT call successful value returned in body",
+              val);
+            },
+            response => {
+              console.log("PUT call in error", response);
+            },
+            () => {
+              this.fields.Valide = "True";
+
+              let alert = this.alertCtrl.create({
+                title: 'Visiteur Enregistrer',
+                subTitle: 'Visiteur Enregistrer !!',
+                buttons: ['Fermer']
+              });
+              alert.present();
+            }
+          );
+
+        }
+
+        UnSavePrence(){
+          let url = `https://api.airtable.com/v0/appRzgYd2sozz8l2P/personne/${this.id}?api_key=keyAER9NsfEje3klJ`;
+          this.funLoading();
+          this.http.put(url,
+            {"fields":{"id":this.fields.id,"Prenom":this.fields.Prenom,"BTS":this.fields.BTS,"Valide":"False","Boisson":this.fields.Boisson,"Nom":this.fields.Nom}})
+            .subscribe(
               val => {
-                  console.log("PUT call successful value returned in body",
-                              val);
+                console.log("PUT call successful value returned in body",
+                val);
               },
               response => {
-                  console.log("PUT call in error", response);
+                console.log("PUT call in error", response);
               },
               () => {
-                this.fields.Valide = "True";
-                console.log(this.fields.Valide);
+                this.fields.Valide = "False";
+
                 let alert = this.alertCtrl.create({
-                  title: 'Visiteur Enregistrer',
-                  subTitle: 'Visiteur Enregistrer !!',
+                  title: 'Visiteur Annuler',
+                  subTitle: 'Visiteur Retirer  !!',
                   buttons: ['Fermer']
                 });
                 alert.present();
-                // fin
               }
-          );
-        // fin test
-  }
+            );
+        }
 
-  saveVestiaire() {
-    console.log(this.Vestiaire);
-  }
+        saveVestiaire() {
+          console.log(this.Vestiaire);
+        }
 
-  refresh(){
-    console.log(this.Vestiaire);
-  }
+        refresh(){
+        }
 
-}
-// {id:100,Nom:"test",Prenom:"test",BTS:"test",BTS:"Valide",Boisson:5}
+      }
+      // {id:100,Nom:"test",Prenom:"test",BTS:"test",BTS:"Valide",Boisson:5}
